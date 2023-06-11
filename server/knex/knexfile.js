6b5ac02e62e30290c731a3e9c5ee0f7f50dcfeda
shlomozion <jordan23zion@gmail.com>
2023-06-11 11:54:22 +0300

    added amazon db

--- server/knex/knexfile.js
+++ server/knex/knexfile.js
@@ -9,10 +9,10 @@ module.exports = {
   staging: {
     client: "mysql",
     connection: {
-      database: "pet_adoption",
+      database: "petAdopt",
       user: "root",
-      password: "",
-      host: "127.0.0.1",
+      password: "Sz$8452752",
+      host: "pet-adopt.cn1g2k9g4tm7.us-east-1.rds.amazonaws.com",
     },
     pool: {
       min: 2,
