bba4ab6cb4996420f3155ad72d6ab4dab49687d6
shlomozion <jordan23zion@gmail.com>
2023-06-11 19:04:02 +0300

    s

--- client/src/Components/LoginSignup/SignUp.jsx
+++ client/src/Components/LoginSignup/SignUp.jsx
@@ -66,7 +66,7 @@ export default function SignUp() {
     if (isValidated === true) {
       try {
         const res = await axios.post(
-          "https://pet-adopt-gamma.vercel.app/users/signup",
+          "https://server-swart-tau.vercel.app/users/signup",
           {
             firstName,
             lastName,
