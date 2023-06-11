9f8c6d38b72de068d2a07609bb279ee2d7ba7c22
shlomozion <jordan23zion@gmail.com>
2023-06-11 17:51:43 +0300

    a

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "https://pet-adopt-shlomozion.vercel.app",
+    origin: "https://pet-adopt-gamma.vercel.app",
     credentials: true,
   })
 );
