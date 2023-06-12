7492c7470051584eb1237562a859b4a721b834f2
shlomozion <jordan23zion@gmail.com>
2023-06-12 19:14:35 +0300

    a

--- server/MiddleWare/usersMiddleware/Signup/DuplicateUserPreventer.js
+++ server/MiddleWare/usersMiddleware/Signup/DuplicateUserPreventer.js
@@ -3,8 +3,9 @@ const database = require("../../../knex/knex");
 const duplicateUserPreventer = async (req, res, next) => {
   try {
     const { email } = req.body;
-    // console.log("req", req.body);
-    const response = await database("users").where({ email: email });
+    console.log("req", req.body);
+    const response = await database("users");
+    // .where({ email: email });
     console.log("response:", response);
 
     !response[0]?.email
