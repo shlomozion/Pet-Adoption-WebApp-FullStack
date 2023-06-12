5055df4f72d5731dd7ee03fdf250d9f720ad530a
shlomozion <jordan23zion@gmail.com>
2023-06-12 23:29:05 +0300

    a

--- server/knex/knexfile.js
+++ server/knex/knexfile.js
@@ -1,22 +1,29 @@
-// Update with your config settings.
 const path = require("path");
 const migrationPath = path.resolve(__dirname, "./migrations");
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
+const serverless = require("serverless-mysql");
+const db = serverless({
+  config: {
+    host: "pet-adopt.cn1g2k9g4tm7.us-east-1.rds.amazonaws.com",
+    user: "root",
+    password: "Sz$8452752",
+    database: "petAdopt",
+  },
+});
+
 module.exports = {
   staging: {
     client: "mysql",
     connection: {
-      database: "petAdopt",
-      user: "root",
-      password: "Sz$8452752",
-      host: "pet-adopt.cn1g2k9g4tm7.us-east-1.rds.amazonaws.com",
+      acquireConnectionTimeout: 10000,
+      createConnection: () => db.promise(),
     },
     pool: {
-      min: 2,
-      max: 10,
+      afterCreate: (conn, done) => {
+        conn.query('SET time_zone = "+00:00";', (err) => {
+          done(err, conn);
+        });
+      },
     },
     migrations: {
       tableName: "knex_migrations",
@@ -27,14 +34,15 @@ module.exports = {
   production: {
     client: "mysql",
     connection: {
-      database: "petAdopt",
-      user: "root",
-      password: "Sz$8452752",
-      host: "pet-adopt.cn1g2k9g4tm7.us-east-1.rds.amazonaws.com",
+      acquireConnectionTimeout: 10000,
+      createConnection: () => db.promise(),
     },
     pool: {
-      min: 2,
-      max: 10,
+      afterCreate: (conn, done) => {
+        conn.query('SET time_zone = "+00:00";', (err) => {
+          done(err, conn);
+        });
+      },
     },
     migrations: {
       tableName: "knex_migrations",
