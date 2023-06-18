abb5c85d3adfacc3bae4074165a6700bbcb391d2
shlomozion <jordan23zion@gmail.com>
2023-06-18 22:13:41 +0300

    a

--- server/MiddleWare/usersMiddleware/Login/CookieIdRetriever.js
+++ server/MiddleWare/usersMiddleware/Login/CookieIdRetriever.js
@@ -3,29 +3,20 @@ const CookieIdRetriever = (req, res, next) => {
   // console.log("req.body", req.body);
   try {
     const rawCookie = req.headers.cookie;
-
     if (!rawCookie) {
       console.log("!rawCookie");
+      res.send("not logged in");
       return;
     } else {
       let arr = rawCookie.split(";");
       const regex = /@([0-9]+)/;
       const [cookieStr] = arr.filter((str) => str.match(regex));
-
-      if (!cookieStr.length) {
-        console.log("!cookieStr.length");
-
-        res.send("not logged in");
-        return;
-      } else {
-        const data = cookieStr.split("=");
-        const id = data[0].split("@");
-        const retrievedCookie = { id: id[1], token: data[1] };
-        // console.log("ret", retrievedCookie);
-        req.body.cookie = retrievedCookie;
-      }
+      const data = cookieStr.split("=");
+      const id = data[0].split("@");
+      const retrievedCookie = { id: id[1], token: data[1] };
+      // console.log("ret", retrievedCookie);
+      req.body.cookie = retrievedCookie;
     }
-
     next();
   } catch (err) {
     console.log(err);
