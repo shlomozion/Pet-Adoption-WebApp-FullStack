505fba5fbe33c041f6fbbd59d79f0385d778df1d
shlomozion <jordan23zion@gmail.com>
2023-06-21 23:01:38 +0300

    login url

--- client/src/Components/LoginSignup/Login.jsx
+++ client/src/Components/LoginSignup/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     if (isEmailValid && isPasswordValid) {
       try {
         const res = await axios.post(
-          `http://localhost:8000/users/login`,
+          `https://pet-adopt-server.vercel.app/users/login`,
           {
             email,
             password,
