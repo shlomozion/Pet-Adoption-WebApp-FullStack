621fdb5a75f6db06d1f7e1c667207007956b42a9
shlomozion <jordan23zion@gmail.com>
2023-06-22 20:34:55 +0300

    a

--- server/Controllers/userControllers.js
+++ server/Controllers/userControllers.js
@@ -61,22 +61,22 @@ const getUserById = async (req, res) => {
       return;
     }
     delete userFromDB.password;
-    const userImage = await getUserProfileImageModel(req);
+    // const userImage = await getUserProfileImageModel(req);
     // console.log("userImg", userImage);
     // console.log(req.body);
-    if (!userImage) {
-      res.send({
-        token,
-        userFromDB,
-      });
-    } else {
-      const key = userImage.key;
-      res.send({
-        key,
-        token,
-        userFromDB,
-      });
-    }
+    // if (!userImage) {
+    //   res.send({
+    //     token,
+    //     userFromDB,
+    //   });
+    // } else {
+    //   const key = userImage.key;
+    //   res.send({
+    //     key,
+    //     token,
+    //     userFromDB,
+    //   });
+    // }
   } catch (err) {
     res.status(500);
   }
